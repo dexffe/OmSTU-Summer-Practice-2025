2739ca42a1f834cfcd8cb6c4caa1866758ba49c2
dexffe <dexffe@gmail.com>
2025-06-24 15:47:54 +0600

    feat: add IsPalindrome extension method

---
+++ task01/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace task01;
+
+public static class StringExtensions
+{ 
+    public static bool IsPalindrome(this string s)
+    {
+        s = new String(s.Where(c => !char.IsPunctuation(c) && !char.IsWhiteSpace(c)).ToArray());
+        return s == new string(s.Reverse().ToArray());
+    }
+}
+    
