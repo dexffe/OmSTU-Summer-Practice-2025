8546d5b453d0153f45452b972bea49074a82c502
dexffe <dexffe@gmail.com>
2025-06-27 11:55:16 +0600

    feat(Spaceships): add spaceship hierarchy with Fighter and Cruiser - Defined Spaceship interface with MoveForvard, Rotate, Fire and FirePower methods - Added Fighter class with speed=100 and firepower=1 - Added Cruiser class with speed=50 and firepower=100

---
+++ task04/Class1.cs
@@ -0,0 +1,35 @@
+﻿namespace task04;
+
+public interface ISpaceship
+{
+    void MoveForward();     
+    void Rotate(int angle); 
+    void Fire();           
+    int Speed { get; }      
+    int FirePower { get; }   
+}
+
+public class Fighter : ISpaceship
+{
+    public void MoveForward() => Console.WriteLine("Истребитель: Движение впред");
+
+    public void Rotate(int angel) =>Console.WriteLine("Истребитель: Поворот на угол (градусы) " + angel);
+
+    public void Fire() =>Console.WriteLine("Истребитель: Выстрел ракетой (слабый)");
+
+    public int Speed { get;  } = 100;
+    public int FirePower { get; } = 1;
+}
+
+
+public class Cruiser : ISpaceship
+{
+    public void MoveForward() => Console.WriteLine("Крейсер: Движение впред");
+
+    public void Rotate(int angel) =>Console.WriteLine("Крейсер: Поворот на угол (градусы) " + angel);
+
+    public void Fire() =>Console.WriteLine("Крейсер: Выстрел ракетой (сильный)");
+
+    public int Speed { get;  } = 50;
+    public int FirePower { get; } = 100;
+}
