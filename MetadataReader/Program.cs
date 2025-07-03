6e4cdb9ea5caaed6efcbe9103f3ef845d7fa2cb5
dexffe <dexffe@gmail.com>
2025-07-03 22:07:00 +0600

    feat(reflection): implement metadata extraction from external assemblies

---
+++ MetadataReader/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace MetadataReader;
+using System;
+using System.Reflection;
+using task07;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Path to assembly is not specified.");
+            return;
+        } 
+        Console.WriteLine($"Assembly: {args[0]}");
+        Assembly assembly = Assembly.LoadFile(args[0]);
+
+        foreach (Type type in assembly.GetTypes())
+        {
+            string className = type.Name;
+            Console.WriteLine($"\nClass: {className}");
+
+            ReflectionHelper.PrintTypeInfo(type);
+            
+            Console.WriteLine();
+        }
+    }
+}
