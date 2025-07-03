da229f702dc68dac0855c70c09c03d6059dd2e53
dexffe <dexffe@gmail.com>
2025-07-03 18:57:29 +0600

    test(commands): add xUnit tests for directory size and file search commands

---
+++ task08tests/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿namespace task08tests;
+
+using System.IO;
+using Xunit;
+using CommandLib;
+using FileSystemCommands;
+
+
+public class FileSystemCommandsTests
+{
+    [Fact]
+    public void DirectorySizeCommand_ShouldCalculateSize()
+    {
+        var testDir = Path.Combine(Path.GetTempPath(), "TestDir");
+        Directory.CreateDirectory(testDir);
+        File.WriteAllText(Path.Combine(testDir, "test1.txt"), "Hello");
+        File.WriteAllText(Path.Combine(testDir, "test2.txt"), "World");
+
+        var command = new DirectorySizeCommand(testDir);
+        command.Execute(); // Проверяем, что не возникает исключений
+
+        Assert.Equal(10, command.dirSize);
+
+        Directory.Delete(testDir, true);
+    }
+
+    [Fact]
+    public void FindFilesCommand_ShouldFindMatchingFiles()
+    {
+        var testDir = Path.Combine(Path.GetTempPath(), "TestDir");
+        Directory.CreateDirectory(testDir);
+        File.WriteAllText(Path.Combine(testDir, "file1.txt"), "Text");
+        File.WriteAllText(Path.Combine(testDir, "file2.log"), "Log");
+
+        var command = new FindFilesCommand(testDir, "*.txt");
+        command.Execute(); // Должен найти 1 файл
+
+        Assert.Single(command.patternFiles);
+
+        Directory.Delete(testDir, true);
+    }
+    
+}
