8f1bb55d045f1e4972c56496a23fec572c327613
dexffe <dexffe@gmail.com>
2025-07-03 18:52:22 +0600

    feat(commands): add DirectorySizeCommand and FindFilesCommand with file system logic - Implemented command to calculate directory size recursively - Implemented command to find files by pattern using SearchOption.AllDirectories - Both commands use ICommand interface for extensibility

---
+++ FileSystemCommands/Class1.cs
@@ -0,0 +1,44 @@
+﻿namespace FileSystemCommands;
+
+using CommandLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public class DirectorySizeCommand : ICommand
+{
+    private string dirPath;
+    public long dirSize = 0;
+    public DirectorySizeCommand(string directoryPath)
+    {
+        this.dirPath = directoryPath;
+    }
+    public void Execute()
+    {
+        if (!Directory.Exists(this.dirPath)) return;
+
+        dirSize = Directory.EnumerateFiles(this.dirPath, "*", SearchOption.AllDirectories)
+            .Sum(file => new FileInfo(file).Length);
+    }
+}
+
+public class FindFilesCommand : ICommand
+{
+    private string dirPath;
+    private string pattern;
+    public List<string> patternFiles;
+
+    public FindFilesCommand(string directoryPath, string searchPattern)
+    {
+        this.dirPath = directoryPath;
+        this.pattern = searchPattern;
+    }
+
+    public void Execute()
+    {
+        if (!Directory.Exists(this.dirPath)) return;
+
+        patternFiles = Directory.EnumerateFiles(this.dirPath, this.pattern, SearchOption.AllDirectories).ToList();
+    }
+}
