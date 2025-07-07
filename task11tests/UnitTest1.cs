253aedc5f8c768f6d10633fedcb87da9ee9d5f5a
dexffe <dexffe@gmail.com>
2025-07-07 18:42:19 +0600

    test: add test for dynamic calculator

---
+++ task11tests/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿namespace task11tests;
+
+using Xunit;
+using task11;
+using System;
+using System.Collections.Generic;
+
+public class TestCalculator
+{
+
+    [Fact]
+    public void AddTest()
+    {
+        var calculator = Calculator.Main();
+
+        var result = calculator?.Add(5, 35);
+        Assert.Equal(40, result);
+    }
+
+    [Fact]
+    public void MinusTest()
+    {
+        var calculator = Calculator.Main();
+
+        var result = calculator?.Minus(4, 4);
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void MulTest()
+    {
+        var calculator = Calculator.Main();
+
+        var result = calculator?.Mul(3, 4);
+        Assert.Equal(12, result);
+    }
+
+    [Fact]
+    public void DivTest()
+    {
+        var calculator = Calculator.Main();
+
+        var result = calculator?.Div(30, 3);
+        Assert.Equal(10, result);
+    }
+}
