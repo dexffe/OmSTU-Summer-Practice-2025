1b518a4bfc83a14d2660c8bc06659d50a435e306
dexffe <dexffe@gmail.com>
2025-07-10 00:22:29 +0600

    fix(test): create const TestFileName

--- task13tests/UnitTest1.cs
+++ task13tests/UnitTest1.cs
@@ -14,24 +14,26 @@ using System.Collections.Generic;
 
 public class StudentJsonTest
 {
+    private const string TestFileName = "studentTest.json";
+    private readonly Student studentTest = new Student
+    {
+        FirstName = "Артём",
+        LastName = null,
+        BirthDate = new DateTime(2006, 9, 23),
+        Grades = new List<Subject>
+        {
+            new Subject { Name = "Математика", Grade = 5 },
+            new Subject { Name = "Физика", Grade = 4 },
+            new Subject { Name = "Английский язык", Grade = 3 },
+            new Subject { Name = "Китайский язык", Grade = 2 }
+        }
+    };
+
+
     [Fact]
     public void Serialize_Test()
     {
-        var student = new Student
-        {
-            FirstName = "Артём",
-            LastName = null,
-            BirthDate = new DateTime(2006, 9, 23),
-            Grades = new List<Subject>
-            {
-                new Subject { Name = "Математика", Grade = 5 },
-                new Subject { Name = "Физика", Grade = 4 },
-                new Subject { Name = "Английский язык", Grade = 3 },
-                new Subject { Name = "Китайский язык", Grade = 2 }
-            }
-        };
-
-        var json = WorkJSON.Serialize(student);
+        var json = WorkJSON.Serialize(studentTest);
 
         Assert.Contains("Артём", json);
         Assert.DoesNotContain("Мартын", json);
@@ -45,20 +47,7 @@ public class StudentJsonTest
     [Fact]
     public void Deserialize_Test()
     {
-        var json = WorkJSON.Serialize(new Student
-        {
-            FirstName = "Артём",
-            LastName = null,
-            BirthDate = new DateTime(2006, 9, 23),
-            Grades = new List<Subject>
-            {
-                new Subject { Name = "Математика", Grade = 5 },
-                new Subject { Name = "Физика", Grade = 4 },
-                new Subject { Name = "Английский язык", Grade = 3 },
-                new Subject { Name = "Китайский язык", Grade = 2 }
-            }
-        });
-
+        var json = WorkJSON.Serialize(studentTest);
         var student = WorkJSON.Deserialize(json);
 
         Assert.Equal("Артём", student.FirstName);
@@ -70,21 +59,9 @@ public class StudentJsonTest
     [Fact]
     public void toFile_Test()
     {
-        var student = new Student
-        {
-            FirstName = "Артём",
-            LastName = null,
-            BirthDate = new DateTime(2006, 9, 23),
-            Grades = new List<Subject>
-            {
-                new Subject { Name = "Математика", Grade = 5 },
-                new Subject { Name = "Физика", Grade = 4 },
-                new Subject { Name = "Английский язык", Grade = 3 },
-                new Subject { Name = "Китайский язык", Grade = 2 }
-            }
-        };
+        if (File.Exists("studentTest.json")) File.Delete("studentTest.json");
 
-        WorkJSON.toFile(WorkJSON.Serialize(student), "studentTest.json");
+        WorkJSON.toFile(WorkJSON.Serialize(studentTest), "studentTest.json");
 
         Assert.True(File.Exists("studentTest.json"));
     }
@@ -92,6 +69,9 @@ public class StudentJsonTest
     [Fact]
     public void fromFile_Test()
     {
+        var json = WorkJSON.Serialize(studentTest);
+        WorkJSON.toFile(json, "studentTest.json");
+
         var student = WorkJSON.Deserialize(WorkJSON.fromFile("studentTest.json"));
         
         Assert.Equal("Артём", student.FirstName);
