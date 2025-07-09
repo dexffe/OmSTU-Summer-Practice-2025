cbfc308b0f31443f68701c1c98100770dc22e35a
dexffe <dexffe@gmail.com>
2025-07-09 14:16:43 +0600

    ...

--- task11/Class1.cs
+++ task11/Class1.cs
@@ -1,51 +1,51 @@
-﻿using System;
-using System.CodeDom;
-using System.CodeDom.Compiler;
-using Microsoft.CSharp;
+﻿namespace task11;
 
-namespace task11
+using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+public interface ICalculator
 {
-    public interface ICalculator
-    {
-        int Add(int a, int b);
-        int Minus(int a, int b);
-        int Mul(int a, int b);
-        int Div(int a, int b);
-    }
+    int Add(int a, int b);
+    int Minus(int a, int b);
+    int Mul(int a, int b);
+    int Div(int a, int b);
+}
 
-    public class Calculator
+public class Calculator
+{
+    public static ICalculator Main()
     {
-        public static ICalculator? Main()
+        string calculatorCode = @"public class Calculator : ICalculator
+                                {
+                                    public int Add(int a, int b) => a + b;
+                                    public int Minus(int a, int b) => a - b;
+                                    public int Mul(int a, int b) => a * b;
+                                    public int Div(int a, int b) => a / b;
+                                }";
+
+        var syntaxTree = CSharpSyntaxTree.ParseText(calculatorCode);
+
+        var refs = new[]
         {
-            string strCode = @"
-                    public class Calculator
-                    {
-                        public int Add(int a, int b) => a + b;
-                        public int Minus(int a, int b) => a - b;
-                        public int Mul(int a, int b) => a * b;
-                        public int Div(int a, int b) => a / b;
-                    }";
+            MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+            MetadataReference.CreateFromFile(typeof(ICalculator).Assembly.Location)
+        };
 
-            var provider = new CSharpCodeProvider();
+        var compilation = CSharpCompilation.Create("CalculatorAssembly", new[] { syntaxTree }, refs,
+                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
 
-            var parameters = new CompilerParameters
-            {
-                GenerateInMemory = true,
-                GenerateExecutable = false
-            };
+        using var ms = new MemoryStream();
+        var emitResult = compilation.Emit(ms);
 
-            parameters.ReferencedAssemblies.Add(typeof(ICalculator).Assembly.Location);
+        ms.Seek(0, SeekOrigin.Begin);
 
-            var res = provider.CompileAssemblyFromSource(parameters, strCode);
+        var assembly = Assembly.Load(ms.ToArray());
 
-            var calcType = res.CompiledAssembly.GetType("Calculator");
-            if (calcType != null)
-            {
-                var calculator = (ICalculator?)Activator.CreateInstance(calcType);
-                return calculator;
-            }
+        var calcType = assembly.GetType("Calculator");
 
-            return null;
-        }
+        return (ICalculator)Activator.CreateInstance(calcType);
     }
 }
--- task11tests/UnitTest1.cs
+++ task11tests/UnitTest1.cs
@@ -7,40 +7,10 @@ using System.Collections.Generic;
 
 public class TestCalculator
 {
-
     [Fact]
-    public void AddTest()
+    public void TestAdd()
     {
         var calculator = Calculator.Main();
-
-        var result = calculator?.Add(5, 35);
-        Assert.Equal(40, result);
-    }
-
-    [Fact]
-    public void MinusTest()
-    {
-        var calculator = Calculator.Main();
-
-        var result = calculator?.Minus(4, 4);
-        Assert.Equal(0, result);
-    }
-
-    [Fact]
-    public void MulTest()
-    {
-        var calculator = Calculator.Main();
-
-        var result = calculator?.Mul(3, 4);
-        Assert.Equal(12, result);
-    }
-
-    [Fact]
-    public void DivTest()
-    {
-        var calculator = Calculator.Main();
-
-        var result = calculator?.Div(30, 3);
-        Assert.Equal(10, result);
+        Assert.Equal(3, calculator.Add(1, 2));
     }
 }
